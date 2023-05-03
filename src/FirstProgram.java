9fcb412554b58c24f0fe843ea95fea41a834677a
Rohini <rohinidsagre15@gmail.com>
2023-05-03 11:30:10 +0530

    assignment 1 added

---
+++ src/FirstProgram.java
@@ -0,0 +1,10 @@
+/*Write a java program to print the 
+ * Velocity Corporate Training Centre
+ * message on console*/
+ 
+public class FirstProgram {
+	 public static void main(String[] args) {
+		 System.out.println("Velocity Corporate Training Centre");
+	 }
+	 
+}
---
+++ src/Message.java
@@ -0,0 +1,10 @@
+/*     
+ * Write a Java Program to print Velocity Corporate training
+ * center message on console.
+ */
+public class Message {
+	public static void main(String[] args) {
+		System.out.println("Velocity Corporate training center");
+	}
+
+}
